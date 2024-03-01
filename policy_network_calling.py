87706ba2d100283932ceb1c74939affbeed68abe
Jasper <halvorsonjasper11@gmail.com>
2024-02-29 17:56:11 -0800

    Neural Network capable of making a prediction

--- policy_network_calling.py
+++ policy_network_calling.py
@@ -7,7 +7,7 @@ model.load_state_dict(torch.load('policy_network_model.pth'))
 model.eval()  # Set the model to evaluation mode
 
 # Assuming you have an input_board as a torch tensor
-input_board = torch.tensor([1, 1, 0, 0, 0, 0, 0, 0, 0], dtype=torch.float32)
+input_board = torch.tensor([[1, 1, 0, 0, 0, 0, 0, 0, 0]], dtype=torch.float32)
 
 # Make predictions using the loaded model
 with torch.no_grad():
